aa99fa95818f96808d25e6e6a3b482c98aab6e5a
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2024-11-09 14:34:39 -0300

    Include jwt secret environment variable

--- apps/backend/src/env.ts
+++ apps/backend/src/env.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 
 const EnvSchema = z.object({
   DATABASE_URL: z.string().startsWith('postgres://'),
+  JWT_SECRET: z.string().min(15),
 });
 
 export const env = EnvSchema.parse(process.env);
