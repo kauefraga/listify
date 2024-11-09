7f4660a352cb40fd67f3f41e2c1ac2417405c03d
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2024-11-09 15:31:50 -0300

    Create user auth route

--- apps/backend/src/user/user.controller.ts
+++ apps/backend/src/user/user.controller.ts
@@ -1,4 +1,5 @@
-import { hash } from 'argon2';
+import { hash, verify } from 'argon2';
+import { eq, or } from 'drizzle-orm';
 import jwt from 'jsonwebtoken';
 import { z } from 'zod';
 import { db } from '../database';
@@ -9,6 +10,12 @@ import { defineController } from '../server';
 const CreateUserSchema = z.object({
   name: z.string().max(50),
   email: z.string().email(),
+  password: z.string(),
+});
+
+const AuthUserSchema = z.object({
+  name: z.string().max(50).optional(),
+  email: z.string().email().optional(),
   password: z.string(),
 });
 
@@ -33,6 +40,43 @@ export const UserController = defineController(http => {
 
     return reply.status(201).send({
       user: newUser,
+      token,
+    });
+  });
+
+  http.post('/v1/user/auth', async (request, reply) => {
+    const { name, email, password } = AuthUserSchema.parse(request.body);
+
+    const isIdentifierMissing = !(name || email);
+    if (isIdentifierMissing) {
+      return reply.status(400).send({
+        message: 'An user identifier should be specified at least, name or e-mail.',
+      });
+    }
+
+    const [existingUser] = await db
+      .select()
+      .from(user)
+      .where(or(eq(user.name, name ?? ''), eq(user.email, email ?? '')))
+      .limit(1);
+
+    if (!existingUser) {
+      return reply.status(400).send({
+        message: 'The user does not exist.',
+      });
+    }
+
+    const isPasswordMatching = await verify(existingUser.password, password);
+    if (!isPasswordMatching) {
+      return reply.status(400).send({
+        message: 'The password does not match.',
+      });
+    }
+
+    const token = jwt.sign({ id: existingUser.id, name: existingUser.name }, env.JWT_SECRET, { expiresIn: '1d' });
+
+    return reply.status(200).send({
+      user: { ...existingUser, password: undefined },
       token,
     });
   });
