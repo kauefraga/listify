6c3e7871761dd874f9060ed813aa2ed1c137d23f
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2024-11-09 14:27:58 -0300

    Add argon2 and implement user creation route

--- apps/backend/src/user/user.controller.ts
+++ apps/backend/src/user/user.controller.ts
@@ -1,7 +1,32 @@
+import { hash } from 'argon2';
+import { z } from 'zod';
+import { db } from '../database';
+import { user } from '../database/schema';
 import { defineController } from '../server';
 
+const CreateUserSchema = z.object({
+  name: z.string().max(50),
+  email: z.string().email(),
+  password: z.string(),
+});
+
 export const UserController = defineController(http => {
-  http.get('/v1/user/create', (request, reply) => {
-    return reply.status(201).send({ message: 'Usuário criado com sucesso.' });
+  http.post('/v1/user/create', async (request, reply) => {
+    const { name, email, password } = CreateUserSchema.parse(request.body);
+
+    const passwordHash = await hash(password);
+
+    const [newUser] = await db.insert(user).values({ name, email, password: passwordHash }).returning({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+    });
+
+    if (!newUser) {
+      return reply.status(400).send({ message: 'Failed to create user.' });
+    }
+
+    return reply.status(201).send(newUser);
   });
 });
