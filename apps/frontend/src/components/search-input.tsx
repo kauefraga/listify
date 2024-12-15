199853adf6d18afaf24e634bac34f236995d3a12
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2024-12-15 10:13:17 -0300

    frontend: improve search input component
    * Use label
    * Set input id

--- apps/frontend/src/components/search-input.tsx
+++ apps/frontend/src/components/search-input.tsx
@@ -4,9 +4,13 @@ import type { InputHTMLAttributes } from 'react';
 export function SearchInput({ ...props }: InputHTMLAttributes<HTMLInputElement>) {
   return (
     <div className="-translate-x-5 flex items-center">
-      <Search className="translate-x-10" />
+      <label htmlFor="search">
+        <Search className="translate-x-10" />
+      </label>
+
       <input
         type="text"
+        id="search"
         className="w-full max-w-96 rounded border-2 border-transparent bg-black/5 px-14 py-3 shadow outline-none transition-colors hover:border-listify-pink focus:border-listify-dark-pink"
         {...props}
       />
